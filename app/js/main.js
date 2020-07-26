17f7a0c8ea83aead2849852e6588c96a6fdd86fd
Andrey558 <andrey01395@yandex.ru>
2020-07-26 18:50:25 +0300

    Full no adaptiv

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,24 @@
-$(function () {
-	$('.reviews-slaider').slick({
-		infinite: true,
-		dots: true,
-		arrows: true,
-		slidesToShow: 3,
-		slidesToScroll: 1
-	});
+$('.reviews-slaider').slick({
+  dots: true,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  ]
 });
